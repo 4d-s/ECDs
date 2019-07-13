3624cf7e0468a4dde2edd1c7fef47b2afdf99f6b
koku28.3dk@gmail.com <koku28.3dk@gmail.com>
2019-07-13 12:57:27 +0900

    管理者top page 4th

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
- 	root 'admin/homes#top'
+ 	get 'admin' => 'admin/homes#top'
 end
