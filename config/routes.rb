810f99e5d5bf4e1e014cfa5ba5af69ecbb2b27c3
koku28.3dk@gmail.com <koku28.3dk@gmail.com>
2019-07-14 13:55:28 +0900

    [Add]アドレスモデル/コントローラの追加[追記:Add]ユーザビューページ/ユーザコントローラの追加（未完成）

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,28 @@
 Rails.application.routes.draw do
-devise_for :admins, controllers: {
-  sessions:      'admins/sessions',
-  passwords:     'admins/passwords',
-  registrations: 'admins/registrations'
-}
-devise_for :users, controllers: {
-  sessions:      'users/sessions',
-  passwords:     'users/passwords',
-  registrations: 'users/registrations'
-}
-
+  namespace :user do
+    get 'addresses/index'
+    get 'addresses/new'
+    get 'addresses/edit'
+  end
+  get 'addresses/index'
+  get 'addresses/new'
+  get 'addresses/edit'
+  get 'addresses/update'
+  get 'addresses/destroy'
   namespace :admin do
     resources :users, only: [:edit, :update, :index, :show]
   end
   namespace :user do
     get 'homes/top'
   end
+  devise_for :admins
+	devise_for :users
 	root 'user/homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- 	get 'admin' => 'admin/homes#top'
+
+ 	  get 'admin' => 'admin/homes#top'
+    get '/mypage', to: 'user/users#show'
+    get '/mypage/edit', to: 'user/users#edit'
+    patch '/user', to: 'user/users#update'
+    post '/user' , to: 'user/users#create'
 end
