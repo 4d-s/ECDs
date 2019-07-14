eb886962bac098e9f6fd5fefe1838de661c38767
yukasaito718 <bumbumbanana718@gmail.com>
2019-07-14 13:13:43 +0900

    [Add]item_selectモデルとコントローラー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'item_selects/index'
+  get 'item_selects/create'
+  get 'item_selects/update'
+  get 'item_selects/destroy'
   devise_for :admins
 	devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_11_034834) do
+ActiveRecord::Schema.define(version: 2019_07_14_035207) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 2019_07_11_034834) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "select_items", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "user_id"
+    t.integer "item_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
