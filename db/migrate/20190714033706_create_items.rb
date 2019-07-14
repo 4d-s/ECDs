d9d3e079d8a1619c91a285e46060f8a4cf4cffff
miyaokat <miyaokta1234@gmail.com>
2019-07-14 03:40:51 +0000

    [Fix]商品モデルのitem_idカラム削除

--- db/migrate/20190714033706_create_items.rb
+++ db/migrate/20190714033706_create_items.rb
@@ -1,7 +1,6 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.integer :item_id
       t.string :item_name
       t.integer :stock
       t.integer :price
