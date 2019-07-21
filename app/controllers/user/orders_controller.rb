fd31cb4e2f5bb0c7b4858df3458e26994626ba73
Junichi-Suzuki11 <largojs@gmail.com>
2019-07-21 06:45:43 +0000

    [Add]購入画面に購入処理を実装、ordersコントローラのアクションを更新

--- app/controllers/user/orders_controller.rb
+++ app/controllers/user/orders_controller.rb
@@ -1,5 +1,20 @@
 class User::OrdersController < ApplicationController
   def new
+  	@item_selects = current_user.item_selects
+  	@order = Order.new
+  end
+
+  def create
+  	@order = current_user.orders.new(order_params)
+  	@order.user_id = current_user.id
+  	@order.order_last_name = current_user.last_name
+  	@order.order_first_name = current_user.first_name
+  	@order.order_last_name = current_user.last_name
+  	@order.order_phone_number = current_user.phone_number
+  	@order.total = @sum
+  	@order.save
+  	flash[:notice] = "購入が完了しました。お買い上げありがとうございます！"
+  	redirect_to root_path
   end
 
   def adress
@@ -7,4 +22,9 @@ class User::OrdersController < ApplicationController
 
   def index
   end
+
+  private
+    def order_params
+      params.require(:order).permit(:order_address, :delivery_status)
+    end
 end
