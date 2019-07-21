25de6ebcef22b98c666fe0dba055ceaa3a734862
yukasaito718 <bumbumbanana718@gmail.com>
2019-07-21 16:02:12 +0900

    [Start]アーティスト名編集ページ

--- app/controllers/admin/artists_controller.rb
+++ app/controllers/admin/artists_controller.rb
@@ -1,9 +1,16 @@
 class Admin::ArtistsController < ApplicationController
-  def new
-  end
+	def new
+		@artist = Artist.new
+	end
 
-  def create
-  end
+	def create
+		@artist = Artist.new(artist_params)
+		@artist.save
+		redirect_to admin_path
+	end
 
-
+	private
+	def artist_params
+    	params.require(:artist).permit(:artist_name)
+	end
 end
