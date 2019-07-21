17e60c154a525c9e540a3647428c0a3e4cc20c5b
Junichi-Suzuki11 <largojs@gmail.com>
2019-07-21 09:44:51 +0000

    [Update]配送先住所の選択を可能にした。

--- app/controllers/user/orders_controller.rb
+++ app/controllers/user/orders_controller.rb
@@ -5,13 +5,32 @@ class User::OrdersController < ApplicationController
   end
 
   def create
-  	@order = current_user.orders.new(order_params)
+  	@sum = 0
+  	@item_selects = current_user.item_selects
+  	@item_selects.each{|item_select|
+  		@sum += item_select.item.price * item_select.item_count
+	}
+	@sum += 500
+	@order = current_user.orders.new(order_params)
   	@order.user_id = current_user.id
-  	@order.order_last_name = current_user.last_name
-  	@order.order_first_name = current_user.first_name
-  	@order.order_last_name = current_user.last_name
-  	@order.order_phone_number = current_user.phone_number
+  	if params[:order][:address_select] == "0" then
+  		@order.order_last_name = current_user.last_name
+  		@order.order_first_name = current_user.first_name
+  		@order.order_postal_code = current_user.postal_code
+  		@order.order_address = current_user.street_address
+  		@order.order_phone_number = current_user.phone_number
+  	elsif params[:order][:address_select] == "1" then
+  		@address = Address.find(params[:order][:address_id])
+  		@order.order_last_name = @address.last_name
+  		@order.order_first_name = @address.first_name
+  		@order.order_postal_code = @address.postal_code
+  		@order.order_address = @address.street_address
+  		@order.order_phone_number = @address.phone_number
+  	elsif params[:order][:address_select] == "2" then
+  	end
+
   	@order.total = @sum
+  	@order.delivery_status = 1
   	@order.save
   	flash[:notice] = "購入が完了しました。お買い上げありがとうございます！"
   	redirect_to root_path
@@ -25,6 +44,6 @@ class User::OrdersController < ApplicationController
 
   private
     def order_params
-      params.require(:order).permit(:order_address, :delivery_status)
+      params.require(:order).permit(:order_address, :payment)
     end
 end
