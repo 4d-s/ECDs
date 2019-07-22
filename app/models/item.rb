57f4e3c1b9f53585391409f2bff279af23f5785d
yukasaito718 <bumbumbanana718@gmail.com>
2019-07-22 19:40:46 +0900

    [Add]新規商品追加画面DISCのCocoon実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,8 +6,7 @@ class Item < ApplicationRecord
 	has_many :discs, inverse_of: :item
   accepts_nested_attributes_for :discs
 	has_many :item_selects, dependent: :destroy
-  # ALLOWED_PARAMS = [:name, :description, :price]
-
+  ALLOWED_PARAMS = [:item_name, :stock, :price, :is_sold, :image, :order_count, :genre_id, :artist_id, :label_id]
 
   #検索用メソッド（空欄で検索した場合は、一覧を表示する）
   def self.Item_search(keyword)
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,4 @@
 class Song < ApplicationRecord
+	ALLOWED_PARAMS = [:id, :disc_id, :song_number, :song_name]
 	belongs_to :disc, inverse_of: :songs
 end
