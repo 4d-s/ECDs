5f0f28b9a20df919f884689f21b4bd5517593c7a
miyaokat <miyaokta1234@gmail.com>
2019-07-22 09:00:26 +0000

    [Start]管理側ユーザー購入履歴ページ

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,8 +4,15 @@ class Admin::OrdersController < ApplicationController
   end
 
   def show
+  	@user = User.find(params[:id])
+  	@orders = @user.orders
   end
 
   def edit
   end
+
+  private
+  def user_params
+  params.require(:user).permit(:name, :last_name, :first_name, :street_address, :postal_code, :phone_number)
+end
 end
