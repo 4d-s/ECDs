82fbc2aab384fc5b482e431ab3f64738c237e024
miyaokat <miyaokta1234@gmail.com>
2019-07-22 11:32:22 +0000

    [Update]管理側・ユーザー側商品一覧ページ表示修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,7 +1,7 @@
 class Admin::ItemsController < ApplicationController
 
   def index
-  	@items = Item.page(params[:page]).per(3).reverse_order
+  	@items = Item.page(params[:page]).per(12).reverse_order
   end
 
   def show
