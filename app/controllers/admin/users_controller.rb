e9749f7b74500c882dd825bf86b7ac06900a087d
Junichi-Suzuki11 <largojs@gmail.com>
2019-07-24 10:31:42 +0000

    [Merge]developブランチをmerge

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < ApplicationController
-  before_action :authenticate_administrator!
+before_action :authenticate_administrator!
   def index
     @users = User.all
   end
