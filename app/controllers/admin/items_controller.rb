de39de9ac925f14a05a61cccc38084331479d3d5
miyaokat <miyaokta1234@gmail.com>
2019-07-26 15:23:14 +0000

    [Update]管理側商品詳細ページ、一覧ページ修正。エンドユーザー側商品詳細ページ、新規配送先画面を修正

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,7 +1,21 @@
 class Admin::ItemsController < ApplicationController
 before_action :authenticate_administrator!
   def index
-  	@items = Item.page(params[:page]).per(12).reverse_order
+  	 select_item_kind = params[:select_id]
+    #CD名での検索
+    if select_item_kind == "1"
+      @items = Item.page(params[:page]).per(12).reverse_order.Item_search(params[:keyword])
+    #アーティスト名での検索
+  elsif  select_item_kind == "2"
+    @items = Item.page(params[:page]).per(12).reverse_order.Artist_search(params[:keyword])
+    #曲名での検索
+  elsif select_item_kind == "3"
+    @items = Item.page(params[:page]).per(12).reverse_order.Song_search(params[:keyword])
+    #検索せずに全商品を表示
+    else
+      params[:keyword] = ""
+      @items = Item.page(params[:page]).per(12).reverse_order
+    end
   end
 
   def show
