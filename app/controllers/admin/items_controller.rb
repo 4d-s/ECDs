57847deec47ae86b23943103700f06485c101313
koku28.3dk@gmail.com <koku28.3dk@gmail.com>
2019-07-28 21:44:29 +0900

    [Edit]フラッシュメッセージの変更・追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -38,8 +38,8 @@ before_action :authenticate_administrator!
         flash[:notice] = "登録しました"
         redirect_to admin_items_path
       else
-        flash.now[:notice] = "登録情報に不備があります。"
-        render :new
+        flash[:notice] = "登録情報に不備がありました。再入力お願い致します。"
+        redirect_to new_admin_item_path
       end
   end
 
