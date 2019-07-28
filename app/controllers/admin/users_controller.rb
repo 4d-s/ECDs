98267aef68a4c0c37656ab7aeea0f2b9e5abfb5e
koku28.3dk@gmail.com <koku28.3dk@gmail.com>
2019-07-28 14:38:21 +0900

    [Edit]管理者編集ページのジャンル等の追加削除・[Add]管理者側flashメッセージの追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -23,8 +23,13 @@ before_action :authenticate_administrator!
   	  	redirect_to admin_users_path
   	  else
   		@user = User.find(params[:id])
-  		@user.update(user_params)
-  		redirect_to admin_user_path(@user.id)
+  		if @user.update(user_params)
+        flash[:notice] = "プロフィール内容を変更しました。"
+  		  redirect_to admin_user_path(@user.id)
+      else
+        flash[:notice] = "プロフィール内容の変更に失敗しました。"
+        redirect_to edit_admin_user_path(@user.id)
+      end
   	end
   end
 
